29cb21992722960e062a94d802ff7076d3a144e3
Haluk COS <halukcos@gmail.com>
2025-02-19 11:39:00 +0300

    Revert api function

--- app/api/movies/[id]/route.ts
+++ app/api/movies/[id]/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import clientPromise from "@/lib/mongodb";
 import { ObjectId } from "mongodb";
 
-export async function GET(req: NextRequest, params: { id: string }) {
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
   try {
     const client = await clientPromise;
     const db = client.db("sample_mflix");
