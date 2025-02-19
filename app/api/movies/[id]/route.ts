18e9f2ae641c8c81d7357ccba1a69f434387de0c
Haluk COS <halukcos@gmail.com>
2025-02-19 12:42:47 +0300

    Fix api function

--- app/api/movies/[id]/route.ts
+++ app/api/movies/[id]/route.ts
@@ -8,12 +8,15 @@ interface parameters {
   };
 }
 
-export async function GET(req: NextRequest, { params }: parameters) {
+export async function GET(req: NextRequest, params: { id: string }) {
   try {
     const client = await clientPromise;
     const db = client.db("sample_mflix");
 
-    const movie = await db.collection("movies").findOne({ _id: new ObjectId(params.id) });
+    const movie = await db.collection("movies").findOne(
+      { _id: new ObjectId(params.id) },
+      { projection: { _id: 1, title: 1, poster: 1, released: 1, plot: 1, fullplot: 1, genres: 1, directors: 1, writers: 1, cast: 1, countries: 1, awards: 1, imdb: 1 } }
+    );
 
     if (!movie) {
       return NextResponse.json({ error: "Film bulunamadı" }, { status: 404 });
